d1b523bc9f86f70c80dc2790f6defdd1025c75b2
nao215912 <79066275+nao215912@users.noreply.github.com>
2022-07-30 17:14:35 +0900

    fix: block_list insert condition

--- problem1/mysql/sql/0_schema.sql
+++ problem1/mysql/sql/0_schema.sql
@@ -5,7 +5,7 @@ create table `users`
     `name`    varchar(64) default '' not null,
     primary key (`id`)
 );
--- user1 user2
+
 create table  `friend_link`
 (
     `id`       bigint(20) not null auto_increment,
@@ -15,12 +15,13 @@ create table  `friend_link`
     unique (user1_id, user2_id),
     primary key (`id`)
 );
--- user1 user2 block
+
 CREATE TABLE `block_list`
 (
     `id`       bigint(20) NOT NULL AUTO_INCREMENT,
     `blocking_user_id` int(11)    NOT NULL,
     `blocked_user_id` int(11)    NOT NULL,
+    check ( blocking_user_id <> blocked_user_id ),
     UNIQUE (blocking_user_id, blocked_user_id),
     PRIMARY KEY (`id`)
 );
