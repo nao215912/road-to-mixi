289bfe41d81ddf22375c36b68c8b66fed0738080
nao215912 <79066275+nao215912@users.noreply.github.com>
2022-08-03 22:39:31 +0900

    refactor

--- problem1/app/domain/object/user.go
+++ problem1/app/domain/object/user.go
@@ -1,12 +1,5 @@
 package object
 
-//	CREATE TABLE `users` (
-//		`id` bigint(20) NOT NULL AUTO_INCREMENT,
-//		`user_id` int(11) NOT NULL,
-//		`name` varchar(64) DEFAULT '' NOT NULL,
-//	PRIMARY KEY (`id`)
-//	);
-
 type User struct {
 	ID     int    `db:"id" json:"-"`
 	UserID int    `db:"user_id" json:"user_id"`
