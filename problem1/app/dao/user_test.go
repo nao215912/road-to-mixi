7c995efa37a30a646ef450891e9a2bc171fbb7a1
nao215912 <79066275+nao215912@users.noreply.github.com>
2022-08-03 19:09:37 +0900

    add: TestGetFriendListLimitOffset

--- problem1/app/dao/user_test.go
+++ problem1/app/dao/user_test.go
@@ -322,3 +322,71 @@ func TestGetFriendOfFriendListExceptBlockListAndFriendList(t *testing.T) {
 		})
 	}
 }
+
+func TestGetFriendListLimitOffset(t *testing.T) {
+	tests := []struct {
+		name    string
+		queries []string
+		userID  int
+		limit   int
+		offset  int
+		want    []object.User
+		wantErr bool
+	}{
+		{
+			name: "example",
+			queries: []string{
+				`insert into users (id, user_id, name) values (1, 1, "Aaron"), (2, 2, "Adam"), (3, 3, "Adlai")`,
+				`insert into follow_relation (following_user_id, followed_user_id) values (1, 2), (2, 1), (1, 3), (3, 1)`,
+			},
+			userID: 1,
+			limit:  1,
+			offset: 1,
+			want: []object.User{
+				{
+					ID:     3,
+					UserID: 3,
+					Name:   "Adlai",
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "limit",
+			queries: []string{
+				`insert into users (id, user_id, name) values (1, 1, "Aaron"), (2, 2, "Adam"), (3, 3, "Adlai")`,
+				`insert into follow_relation (following_user_id, followed_user_id) values (1, 2), (2, 1), (1, 3), (3, 1)`,
+			},
+			userID: 1,
+			limit:  1,
+			offset: 0,
+			want: []object.User{
+				{
+					ID:     2,
+					UserID: 2,
+					Name:   "Adam",
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u, err := NewTestUser(tt.queries)
+			if err != nil {
+				t.Fatal(err)
+			}
+			got, err := u.GetFriendListLimitOffset(context.Background(), tt.userID, tt.limit, tt.offset)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetFriendListLimitOffset() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetFriendListLimitOffset() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
