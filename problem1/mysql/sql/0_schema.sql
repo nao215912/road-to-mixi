e3cd7c06a685f01532229e89a645ca192af48808
nao215912 <79066275+nao215912@users.noreply.github.com>
2022-08-03 18:54:15 +0900

    refactor

--- problem1/mysql/sql/0_schema.sql
+++ problem1/mysql/sql/0_schema.sql
@@ -3,6 +3,7 @@ create table `users`
     `id`      bigint(20)  not null auto_increment,
     `user_id` bigint(20)  not null,
     `name`    varchar(64) not null,
+    index idx_user_id (user_id),
     primary key (`id`)
 );
 
@@ -11,6 +12,8 @@ create table `follow_relation`
     `following_user_id` int(20) not null,
     `followed_user_id`  int(20) not null,
     check ( following_user_id <> followed_user_id ),
+    index idx_following_user_id (following_user_id),
+    index idx_followed_user_id (followed_user_id),
     primary key (following_user_id, followed_user_id)
 );
 
@@ -19,5 +22,7 @@ CREATE TABLE `block_relation`
     `blocking_user_id` int(20) NOT NULL,
     `blocked_user_id`  int(20) NOT NULL,
     check ( blocking_user_id <> blocked_user_id ),
+    index idx_blocking_user_id (blocking_user_id),
+    index idx_blocked_user_id (blocked_user_id),
     primary key (blocking_user_id, blocked_user_id)
 );
