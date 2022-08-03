60589226c659f1ec50de7b2ee168c6a3bcd90768
nao215912 <79066275+nao215912@users.noreply.github.com>
2022-08-03 19:00:24 +0900

    add: json tag

--- problem1/app/domain/object/user.go
+++ problem1/app/domain/object/user.go
@@ -8,7 +8,7 @@ package object
 //	);
 
 type User struct {
-	ID     int    `db:"id"`
-	UserID int    `db:"user_id"`
-	Name   string `db:"name"`
+	ID     int    `db:"id" json:"-"`
+	UserID int    `db:"user_id" json:"user_id"`
+	Name   string `db:"name" json:"name"`
 }
