c5e582a8fff8a791a426d8eaf82996e715d35d9b
nao215912 <79066275+nao215912@users.noreply.github.com>
2022-08-04 16:26:01 +0900

    refactor

--- problem1/app/domain/repository/user.go
+++ problem1/app/domain/repository/user.go
@@ -5,6 +5,8 @@ import (
 	"minimal_sns/domain/object"
 )
 
+// User
+//	handlerに１体１で対応するmethodを生やしているのが微妙
 type User interface {
 	GetFriendList(ctx context.Context, userID int) ([]object.User, error)
 	GetFriendListLimitOffset(ctx context.Context, userID, limit, offset int) ([]object.User, error)
